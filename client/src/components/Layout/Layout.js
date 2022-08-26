decbc88a98ac1849c3cc840d6c6faa4df6f222a0
smandla <35283559+smandla@users.noreply.github.com>
2022-08-25 18:11:15 -0700

    Updates

--- client/src/components/Layout/Layout.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Layout.module.css";
-const Layout = (props) => {
-  return <div className={classes.layout}>{props.children}</div>;
-};
-
-export default Layout;
